6757b52fe809ea7cc1fc8efb7ccbf6261c8664f8
cg33 <chg80333@163.com>
2020-05-18 10:44:00 +0800

    update to v1.2.11

--- pages/form.go
+++ pages/form.go
@@ -6,15 +6,16 @@ import (
 	"github.com/GoAdminGroup/go-admin/modules/db"
 	"github.com/GoAdminGroup/go-admin/modules/language"
 	form2 "github.com/GoAdminGroup/go-admin/plugins/admin/modules/form"
-	"github.com/GoAdminGroup/go-admin/template"
+	template2 "github.com/GoAdminGroup/go-admin/template"
 	"github.com/GoAdminGroup/go-admin/template/icon"
 	"github.com/GoAdminGroup/go-admin/template/types"
 	"github.com/GoAdminGroup/go-admin/template/types/form"
+	"html/template"
 )
 
 func GetFormContent(ctx *context.Context) (types.Panel, error) {
 
-	components := template.Get(config.GetTheme())
+	components := template2.Get(config.GetTheme())
 
 	col1 := components.Col().GetContent()
 	btn1 := components.Button().SetType("submit").
@@ -36,16 +37,34 @@ func GetFormContent(ctx *context.Context) (types.Panel, error) {
 	panel.AddField("Age", "age", db.Int, form.Number)
 	panel.AddField("HomePage", "homepage", db.Varchar, form.Url).FieldDefault("http://google.com")
 	panel.AddField("Email", "email", db.Varchar, form.Email).FieldDefault("xxxx@xxx.com")
-	panel.AddField("Birthday", "birthday", db.Varchar, form.Datetime).FieldDefault("2010-09-05")
-	panel.AddField("Password", "password", db.Varchar, form.Password)
+	panel.AddField("Birthday", "birthday", db.Varchar, form.Date).FieldDefault("2010-09-03 18:09:05")
+	panel.AddField("Time", "time", db.Varchar, form.Datetime).FieldDefault("2010-09-05")
+	panel.AddField("Time Range", "time_range", db.Varchar, form.DatetimeRange)
+	panel.AddField("Date Range", "date_range", db.Varchar, form.DateRange)
+	panel.AddField("Password", "password", db.Varchar, form.Password).FieldDivider("Divider line")
 	panel.AddField("IP", "ip", db.Varchar, form.Ip)
 	panel.AddField("Certificate", "certificate", db.Varchar, form.Multifile).FieldOptionExt(map[string]interface{}{
 		"maxFileCount": 10,
 	})
 	panel.AddField("Money", "currency", db.Int, form.Currency)
+	panel.AddField("Rate", "rate", db.Int, form.Rate)
+	panel.AddField("Reward", "reward", db.Int, form.Slider).FieldOptionExt(map[string]interface{}{
+		"max":     1000,
+		"min":     1,
+		"step":    1,
+		"postfix": "$",
+	})
 	panel.AddField("Content", "content", db.Text, form.RichText).
-		FieldDefault(`<h1>343434</h1><p>34344433434</p><ol><li>23234</li><li>2342342342</li><li>asdfads</li></ol><ul><li>3434334</li><li>34343343434</li><li>44455</li></ul><p><span style="color: rgb(194, 79, 74);">343434</span></p><p><span style="background-color: rgb(194, 79, 74); color: rgb(0, 0, 0);">434434433434</span></p><table border="0" width="100%" cellpadding="0" cellspacing="0"><tbody><tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr><tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr><tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr><tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr></tbody></table><p><br></p><p><span style="color: rgb(194, 79, 74);"><br></span></p>`)
+		FieldDefault(`<h1>343434</h1><p>34344433434</p><ol><li>23234</li><li>2342342342</li><li>asdfads</li></ol><ul><li>3434334</li><li>34343343434</li><li>44455</li></ul><p><span style="color: rgb(194, 79, 74);">343434</span></p><p><span style="background-color: rgb(194, 79, 74); color: rgb(0, 0, 0);">434434433434</span></p><table border="0" width="100%" cellpadding="0" cellspacing="0"><tbody><tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr><tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr><tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr><tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr></tbody></table><p><br></p><p><span style="color: rgb(194, 79, 74);"><br></span></p>`).
+		FieldDivider("Divider line No2")
+	panel.AddField("Code", "code", db.Text, form.Code).FieldDefault(`package main
 
+import "fmt"
+
+func main() {
+	fmt.Println("hello GoAdmin!")
+}
+`)
 	panel.AddField("Website", "website", db.Tinyint, form.Switch).
 		FieldHelpMsg("The Website will not be able to access after closing, the admin system still can login").
 		FieldOptions(types.FieldOptions{
@@ -81,11 +100,54 @@ func GetFormContent(ctx *context.Context) (types.Panel, error) {
 			{Text: "four years", Value: "2"},
 			{Text: "five years", Value: "3"},
 		}).FieldDefault("beer")
-	panel.SetTabGroups(types.TabGroups{
-		{"name", "age", "homepage", "email", "birthday", "password", "ip", "certificate", "currency", "content"},
-		{"website", "fruit", "gender", "drink", "experience"},
+	panel.AddField("Snacks", "snacks", db.Varchar, form.Checkbox).
+		FieldOptions(types.FieldOptions{
+			{Text: "cereal", Value: "0"},
+			{Text: "chips", Value: "1"},
+			{Text: "spicy strip", Value: "2"},
+			{Text: "ice cream", Value: "3"},
+		})
+	panel.AddField("Cat", "cat", db.Varchar, form.CheckboxStacked).
+		FieldOptions(types.FieldOptions{
+			{Text: "Garfield", Value: "0"},
+			{Text: "British Shorthair", Value: "1"},
+			{Text: "American Shorthair", Value: "2"},
+		})
+	panel.AddRow(func(pa *types.FormPanel) {
+		panel.AddField("Province", "province", db.Tinyint, form.SelectSingle).
+			FieldOptions(types.FieldOptions{
+				{Text: "Beijing", Value: "0"},
+				{Text: "Shanghai", Value: "1"},
+				{Text: "GuangDong", Value: "2"},
+				{Text: "ChongQing", Value: "3"},
+			}).FieldRowWidth(2)
+		panel.AddField("City", "city", db.Tinyint, form.SelectSingle).
+			FieldOptions(types.FieldOptions{
+				{Text: "Beijing", Value: "0"},
+				{Text: "Shanghai", Value: "1"},
+				{Text: "GuangZhou", Value: "2"},
+				{Text: "ShenZhen", Value: "3"},
+			}).FieldRowWidth(3).FieldHeadWidth(2).FieldInputWidth(10)
+		panel.AddField("District", "district", db.Tinyint, form.SelectSingle).
+			FieldOptions(types.FieldOptions{
+				{Text: "ChaoYang", Value: "0"},
+				{Text: "HaiZhu", Value: "1"},
+				{Text: "PuDong", Value: "2"},
+				{Text: "BaoAn", Value: "3"},
+			}).FieldRowWidth(3).FieldHeadWidth(2).FieldInputWidth(9)
 	})
-	panel.SetTabHeaders("input", "select")
+	panel.AddField("Employee", "employee", db.Varchar, form.Array)
+	panel.AddTable("Setting", "setting", func(panel *types.FormPanel) {
+		panel.AddField("Key", "key", db.Varchar, form.Text).FieldHideLabel()
+		panel.AddField("Value", "value", db.Varchar, form.Text).FieldHideLabel()
+	})
+	panel.SetTabGroups(types.TabGroups{
+		{"name", "age", "homepage", "email", "birthday", "time", "time_range", "date_range", "password", "ip",
+			"certificate", "currency", "rate", "reward", "content", "code"},
+		{"website", "snacks", "fruit", "gender", "cat", "drink", "province", "city", "district", "experience"},
+		{"employee", "setting"},
+	})
+	panel.SetTabHeaders("input", "select", "multi")
 
 	fields, headers := panel.GroupField()
 
@@ -100,12 +162,19 @@ func GetFormContent(ctx *context.Context) (types.Panel, error) {
 		}).
 		SetOperationFooter(col1 + col2)
 
+	popup := components.Popup().SetID("code_modal").
+		SetHideFooter().
+		SetTitle("Code").
+		SetHeight("300px").
+		SetBody(template.HTML("")).
+		GetContent()
+
 	return types.Panel{
 		Content: components.Box().
-			SetHeader(aform.GetDefaultBoxHeader()).
+			SetHeader(aform.GetDefaultBoxHeader(true)).
 			WithHeadBorder().
 			SetBody(aform.GetContent()).
-			GetContent(),
+			GetContent() + popup,
 		Title:       "Form",
 		Description: "form example",
 	}, nil
